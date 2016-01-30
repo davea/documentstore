f6051c0970c589de3ccb38b121511b3f9f391961
Dave Arter <dave@minus-zero.org>
2016-01-30 15:03:17 +0000

    Allow Document.other_pages to be empty
    Also don't use a lambda for Document.file.upload_to because it can't be
    serialised and throws an exception.

--- documents/models.py
+++ documents/models.py
@@ -1,20 +1,23 @@
 from django.db import models
 from django.contrib.postgres.fields import JSONField
 
+# Using a function that simply returns the suggested filename allows us to
+# specify its subdir via File().name.
+def document_file_upload_path(instance, filename):
+    return filename
+
 class Document(models.Model):
     # Fields describing the content of the document
     author = models.CharField(max_length=128, blank=True, null=True, help_text="Who wrote/sent/created this document")
     date = models.DateField(blank=True, null=True, help_text="The date printed on the document")
     time = models.TimeField(blank=True, null=True, help_text="The time printed on the document")
     page_number = models.IntegerField(default=1)
-    other_pages = models.ManyToManyField('self')
+    other_pages = models.ManyToManyField('self', blank=True)
 
     # Fields relating to the import and storage of the document
     source = models.CharField(max_length=128, default="Manually imported", help_text="How this document made its way into the system")
     imported = models.DateTimeField(auto_now_add=True)
-    # Using a lambda that simply returns the suggested filename allows us to
-    # specify its subdir via File().name.
-    file = models.FileField(upload_to=lambda i, f: f)
+    file = models.FileField(upload_to=document_file_upload_path)
 
     # Fields specific to documents imported using doxieapi
     doxieapi_scan_json = JSONField(blank=True, null=True)
